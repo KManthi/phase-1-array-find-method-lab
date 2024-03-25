fe7e984083918ceea60182f4cbd8579c26f65a21
Daniel <dani3lkamanthi@gmail.com>
2024-03-25 12:49:00 +0300

    Modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function superbowlWin(record) {
+    const result = record.find(function(record) {
+      return record.result === "W";
+    });
+    return result ? result.year : undefined;
+  }
